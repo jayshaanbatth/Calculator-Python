49c7687d4d2e2f6086bccbbbab1c5b55c8cbd732
jayshaanbatth <jayshaanbatth@gmail.com>
2025-12-31 16:59:48 -0500

    Added descriptions and have solid layout of simple calculator programs

---
+++ calculator.py
@@ -0,0 +1,11 @@
+"""
+Created By: Jayshaan Batth
+Supplement to: The calculator program (python)
+Date: 12/31/25
+Description: This is where the calculation methods are defined to be used in the main file
+"""
+
+class Calcuator:
+
+    def add(self):
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 """
+Created by: Jayshaan Batth
+Supplement to: calculator program (python)
+Date: 12/31/25
+Description: This is the main file that determines user input and conditionals
+
 This is the start of the calcuator program
 1. Setup POOP
 2. Write Code
