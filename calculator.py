15d0021a3ae9312682dd209053d910fb4d397a02
jayshaanbatth <jayshaanbatth@gmail.com>
2026-01-01 19:01:10 -0500

    Added the base calculator functions

--- calculator.py
+++ calculator.py
@@ -7,5 +7,14 @@ Description: This is where the calculation methods are defined to be used in the
 
 class Calcuator:
 
-    def add(self):
-        pass
+    def add(self, num1, num2):
+        return num1 + num2
+
+    def subtract(self, num1, num2):
+        return num1 - num2
+
+    def multiply(self, num1, num2):
+        return num1 * num2
+
+    def divide(self, num1, num2):
+        return num1/num2
